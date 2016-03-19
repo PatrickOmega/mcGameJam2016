2a014a3b796736fe66c1de27811c5f68c4464dce
Patrick <just.pat@hotmail.com>
2016-03-18 21:08:33 -0400

    Created some tower classes

---
+++ TowerDefense/TowerDefense/Data/Models/Towers/TowerType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Data.Models.Towers
+{
+    /// <summary>
+    /// Defines types of towers available for the player to deploy.
+    /// 
+    /// Author - Patrick Nicoll
+    /// Version - Mar. 18, 2016
+    /// </summary>
+    public enum TowerType { TESLA, TYPEB, TYPEC, TYPED}
+   
+}
