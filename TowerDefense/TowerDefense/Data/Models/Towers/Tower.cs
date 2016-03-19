1ab0a58a1295d83af1b9106ed40111f19f39ce84
Patrick <just.pat@hotmail.com>
2016-03-18 22:23:41 -0400

    Started filling out tower info

--- TowerDefense/TowerDefense/Data/Models/Towers/Tower.cs
+++ TowerDefense/TowerDefense/Data/Models/Towers/Tower.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TowerDefense.Data.Models.Viruses;
 
 namespace TowerDefense.Data.Models.Towers
 {
-    class Tower
+    class Tower: Entity
     {
-        private int health;
-        private int cost;
-        private int damage;
-        private float attackSpeed;
-        private int range;
-        private TowerDamageType damageType;
-        private 
+        //variables
+        public String name { get; set; }
+        public int cost { get; set; }
+        public int damage { get; set; }
+        public int attackSpeed { get; set; }
+        public int range { get; set; }
+        public TowerDamageType damageType { get; set; } // Single or area splash
+        public VirusType virusType { get; set; } // Whether a ground or flying unit
+        public int level { get; set; } // 1-5 
+        public int upgradeCost { get; set; }
+
     }
 }
